47a2f851a49106e0096b3cc1b8a826f55f20e4ee
Daniel Hathcock <danielchathcock98@gmail.com>
2022-07-12 15:28:18 -0400

    Fixed add function

--- demos/git-demo.py
+++ demos/git-demo.py
@@ -3,7 +3,7 @@ import random
 
 def dot_product(x, y):
     if len(x) != len(y):
-        raise Exception("Length of vectors do not agree").
+        raise Exception("Length of vectors do not agree")
 
     sum = 0
     for i in range(len(x)):
@@ -11,4 +11,11 @@ def dot_product(x, y):
     return sum
 
 def add(x, y): 
-    return x + y
+    if len(x) != len(y):
+        raise Exception("Length of vectors do not agree")
+
+    z = []
+    for i in range(len(x)):
+        z.append(x[i] + y[i])
+
+    return z
