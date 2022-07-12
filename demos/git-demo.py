10e93a9c882f37e70a94bbef8f19a49d591aa845
Sherry Sarkar <sherrysarkar@sherrys-mbp.wifi.local.cmu.edu>
2022-07-12 14:32:34 -0400

    Demo 1

---
+++ demos/git-demo.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random
+
+def dot_product(x, y):
+  if len(x) != len(y):
+    raise Exception("Length of vectors do not agree").
+
+  sum = 0
+  for i in range(len(x)):
+      sum += x[i] * y[i]
+  return sum
