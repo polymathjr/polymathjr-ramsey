47b776b597962c1436141aef9b02a8e9a9509275
A Basu <81401491+arponbasu@users.noreply.github.com>
2022-07-22 00:34:42 +0530

    Add files via upload

--- pseudo-random/Kopparty.py
+++ pseudo-random/Kopparty.py
@@ -18,23 +18,71 @@ def kopparty(q):
         vec.set_immutable()
     return Graph([V, lambda u, v: isEdge(u, v)])
 
-kp = kopparty(3)
-kp.show()
-print(kp.order(), kp.size(), kp.complement().size())
-
-
-
 with open('r35_13.g6', 'r') as file: # Opens the file, and closes it once we exit this block. 
     graphs = file.readlines() 
-print(graphs)
-g = Graph() # Create an empty graph
-from_graph6(g, graphs[0])
-g.show()
+r35_13 = Graph() # Create an empty graph
+from_graph6(r35_13, graphs[0])
+r35_13.show()
+
+
+
+def isZero(v):
+    for e in v:
+        if e!= 0:
+            return False
+    return True
+def oneDimSubspace(q, dim):
+    try:
+        k = GF(q, repr = 'int') # This gives us the elements of the finite fields as integers
+    except:
+        raise Exception("The order of a finite field must be a prime power")
+    V = list(VectorSpace(k, dim))
+    for v in V:
+        if isZero(v):
+            V.remove(v)
+            break
+    scal_list = [x for _, x in enumerate(k) if (x != 0 and x != 1)] #Non zero, non-one elements of F_q
+    for vec in V:
+        for s in scal_list:
+            if vec*s in V:
+                V.remove(vec*s)
+    for vec in V: vec.set_immutable()
+    return V
+
+
+
+def beta(u, v):
+    if len(u) != len(v) or len(u) % 2 != 0: 
+        raise Exception('Input vectors need to have equal and even dimension.')
+    
+    B = 0
+    for i in range(0, len(u), 2): 
+        B += u[i] * v[i + 1] - u[i + 1] * v[i]
+    
+    return B
+
+def kopparty_alt(q):
+    V = oneDimSubspace(q, 4)
+    return Graph([V, lambda u, v: beta(u, v) == 0])
+
 
 
 kp9 = kopparty(9)
-print("Graph constructed")
-subg3 = kp9.subgraph_search(g, induced=True)
+print(kp9.order(), kp9.size(), kp9.complement.size())
+
+subg3 = kp9.subgraph_search(r35_13, induced=True)
 if subg3 is not None:
     print(subg3.vertices())
+
+
+
+kpa8 = kopparty_alt(8)
+print(kpa8.order(), kpa8.size(), kpa8.complement.size())
+
+
+s1 = kpa8.subgraph_search(r35_13)
+if s1 is not None:
+    print(s1.vertices())
+
+
 
