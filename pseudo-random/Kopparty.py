f390f852d20de91fe84c5b933ab894927d153de0
A Basu <81401491+arponbasu@users.noreply.github.com>
2022-07-22 10:53:35 +0530

    Added comments

--- pseudo-random/Kopparty.py
+++ pseudo-random/Kopparty.py
@@ -10,7 +10,7 @@ def kopparty(q):
     Returns Kopparty Graph defined over F_q
     '''
     try:
-        k = GF(q, repr='int') # This gives us the elements of the finite fields as integers
+        k = GF(q, repr = 'int') # This gives us the elements of the finite fields as integers
     except:
         raise Exception("The order of a finite field must be a prime power")
     V = list(VectorSpace(k, 3)) # This gives us F^3_q
@@ -26,23 +26,32 @@ r35_13.show()
 
 
 
-def isZero(v):
+def isZero(v): 
+    '''
+    Function to recognize if v is the zero vector over F_q.
+    Surely Sage has a function for this, but I couldn't find it
+    '''
     for e in v:
         if e!= 0:
             return False
     return True
+
 def oneDimSubspace(q, dim):
     try:
         k = GF(q, repr = 'int') # This gives us the elements of the finite fields as integers
     except:
         raise Exception("The order of a finite field must be a prime power")
     V = list(VectorSpace(k, dim))
-    for v in V:
+    for v in V: #very ugly hack to remove the zero vector from V
         if isZero(v):
             V.remove(v)
             break
     scal_list = [x for _, x in enumerate(k) if (x != 0 and x != 1)] #Non zero, non-one elements of F_q
     for vec in V:
+        '''
+        For each vector, we remove all of it's scalar multiples from V.
+        The vectors which remain are considered to be representatives of the 1 dimensional subspaces they generate
+        '''
         for s in scal_list:
             if vec*s in V:
                 V.remove(vec*s)
@@ -63,12 +72,12 @@ def beta(u, v):
 
 def kopparty_alt(q):
     V = oneDimSubspace(q, 4)
-    return Graph([V, lambda u, v: beta(u, v) == 0])
+    return Graph([V, lambda u, v: (u != v and beta(u, v) == 0)])
 
 
 
 kp9 = kopparty(9)
-print(kp9.order(), kp9.size(), kp9.complement.size())
+print(kp9.order())
 
 subg3 = kp9.subgraph_search(r35_13, induced=True)
 if subg3 is not None:
@@ -80,7 +89,7 @@ kpa8 = kopparty_alt(8)
 print(kpa8.order())
 
 
-s1 = kpa8.subgraph_search(r35_13)
+s1 = kpa8.subgraph_search(r35_13) #This takes quite some time (4-5 minutes)
 if s1 is not None:
     print(s1.vertices())
 
